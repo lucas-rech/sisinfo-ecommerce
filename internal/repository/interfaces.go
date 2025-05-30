1f5b82969f3370b88ad9437c0fa64891c10bf7ac
Lucas Rech <lucasrech00@gmail.com>
2025-05-30 16:33:46 -0300

    feat: criado repository de usuario

---
+++ internal/repository/interfaces.go
@@ -0,0 +1,11 @@
+package repository
+
+import "github.com/lucas-rech/sisinfo-ecommerce/internal/domain"
+
+type UserRepository interface {
+	Create(user *domain.User) error
+	FindByID(id uint) (*domain.User, error)
+	FindByEmail(email string) (*domain.User, error)
+	Update(user *domain.User) error
+	Delete(id uint) error
+}
---
+++ internal/repository/user_repository.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"github.com/lucas-rech/sisinfo-ecommerce/internal/domain"
+	"gorm.io/gorm"
+)
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db}
+}
+
+
+
+func (r *userRepository) Create(user *domain.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) Delete(id uint) error {
+	return r.db.Delete(&domain.User{}, id).Error
+}
+
+func (r *userRepository) FindByEmail(email string) (*domain.User, error) {
+	var user domain.User
+	err := r.db.Where("email =?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) FindByID(id uint) (*domain.User, error) {
+	var user domain.User
+	err := r.db.First(&user, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) Update(user *domain.User) error {
+	return r.db.Save(user).Error
+}
+
+
+
+
