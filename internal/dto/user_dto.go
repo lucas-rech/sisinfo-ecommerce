2028ef815862b190e08732fc62ea97c99df0075c
Lucas Rech <lucasrech00@gmail.com>
2025-05-31 11:46:08 -0300

    feat: criado dtos de criação e update de usuarío

---
+++ internal/dto/user_dto.go
@@ -0,0 +1,13 @@
+package dto
+
+type UserCreateRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type UserUpdateRequest struct {
+	Email    *string `json:"email"`
+	Name     *string `json:"name"`
+	Password *string `string:"password"`
+}
