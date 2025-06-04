37ea5b23a7378646f40e9b7d2896881ce4e34e68
Lucas Rech <lucasrech00@gmail.com>
2025-06-04 01:18:15 -0300

    refactor: adicionado atributo de tempo ao domínio cart_item

--- backend/internal/domain/cart_item.go
+++ backend/internal/domain/cart_item.go
@@ -1,9 +1,12 @@
 package domain
 
+import "time"
+
 type CartItem struct {
-	ID        uint    `gorm:"primaryKey" json:"id"`
-	CartID    uint    `json:"cart_id"`
-	ProductID uint    `json:"product_id"`
-	Quantity  int     `json:"quantity"`
-	Product   Product `gorm:"foreignKey:ProductID" json:"product"`
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	CartID    uint      `json:"cart_id"`
+	ProductID uint      `json:"product_id"`
+	Quantity  int       `json:"quantity"`
+	Product   Product   `gorm:"foreignKey:ProductID" json:"product"`
+	DateAdded time.Time `gorm:"autoCreateTime" json:"date_added"`
 }
