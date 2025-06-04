3e26bee964c85d9211cf8e1ee46e57814ce63e16
Lucas Rech <lucasrech00@gmail.com>
2025-06-04 01:28:34 -0300

    refactor: implementados middlewares em rotas e organizando endpoints por grupos corretos

--- backend/router/router.go
+++ backend/router/router.go
@@ -8,20 +8,22 @@ import (
 	"github.com/lucas-rech/sisinfo-ecommerce/backend/utils/middleware"
 )
 
-func SetupRouter(productHandler *handler.ProductHandler, userHandler *handler.UserHandler) *gin.Engine {
+func SetupRouter(productHandler *handler.ProductHandler, userHandler *handler.UserHandler, cartItemHandler *handler.CartItemHandler) *gin.Engine {
 	router := gin.Default()
 
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 	})
 
-	v1 := router.Group("/api/v1")
+	routes := router.Group("/api/v1")
 	{
 
-		admin := v1.Group("/admin")
+		admin := routes.Group("/admin")
 		{
+			// Middlewares para autenticação
+			admin.Use(middleware.JWTAuthAdmin())
+
 			// Rotas de produtos
-			admin.Use(middleware.JWTAuth())
 			admin.POST("/product", productHandler.CreateProduct)
 			admin.PATCH("/product/:id", productHandler.UpdateProduct)
 			admin.DELETE("/product/:id", productHandler.DeleteProduct)
@@ -34,17 +36,30 @@ func SetupRouter(productHandler *handler.ProductHandler, userHandler *handler.Us
 
 		}
 
-		consumer := v1.Group("/")
+		consumer := routes.Group("/")
 		{
-			consumer.Use(middleware.JWTAuthCustomer())
-			consumer.GET("/product/:id", productHandler.FindProductByID)
-			consumer.GET("/products", productHandler.FindAllProducts)
+			// Middlewares para autenticação e extração de claims
+			consumer.Use(middleware.JWTAuthCustomer(), middleware.ExtractClaims())
+
+			consumer.POST("/cart/item", cartItemHandler.AddItemToCart)
+			consumer.PATCH("/cart/item", cartItemHandler.UpdateItemInCart)
+			consumer.DELETE("/cart/item", cartItemHandler.RemoveItemFromCart)
+			consumer.GET("/cart/items", cartItemHandler.GetAllItemsInCart)
 
 		}
 
-		// Rotas de autenticação
-		v1.POST("/login", userHandler.Login)
-		v1.POST("/user", userHandler.CreateUser)
+		open := routes.Group("/")
+		{
+			// Rotas de login e registro de usuários
+			open.POST("/login", userHandler.Login)
+			open.POST("/login/register", userHandler.CreateUser)
+
+
+			// Rotas de produtos
+			open.GET("/product/:id", productHandler.FindProductByID)
+			open.GET("/products", productHandler.FindAllProducts)
+		}
+
 	}
 
 	return router
